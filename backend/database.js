8507fb00f16c234d67e9c7b2a686698e8d354515
Dwhitewolf1995 <70945889+Dwhitewolf1995@users.noreply.github.com>
2022-05-30 20:16:16 +0200

    Fix for heroku

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,13 @@
 const admin = require("firebase-admin");
 
+let serviceAccount;
+if (process.env.PRIVATE_KEY) {
+  serviceAccount = JSON.parse(process.env.PRIVATE_KEY)
+}
+else {
+  serviceAccount = require("./hamsterwars-c14e5-firebase-adminsdk-pzr48-b275c2e3cd.json");
+}
 
-
-const serviceAccount = require("./hamsterwars-c14e5-firebase-adminsdk-pzr48-b275c2e3cd.json");
 
 
 
